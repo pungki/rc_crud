5fdea0000093f7b813d23703a3e886342d9db0f6
wiwik <witwoeight@ymail.com>
2013-12-18 17:23:04 +0700

    penambahan css index

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
-
+<link rel="stylesheet" media="all" href="cuantik.css" />
+<div align="center">
 	<form class="form" method="post" action="proseslogin.php">
 	    <div> <h2>Login Admin</h2></div>
-		<div><input type="text" name="username" placeholder="Username" required></div>
-		 <div><input type="password" name="password" placeholder="Password" required> </div>
+		<div><input type="text" size:"30" name="username" placeholder="Username" required></div>
+		 <div><input type="password" size:"30" name="password" placeholder="Password" required> </div>
+		 <br/>
+		 
 	<div> <input type="submit" value="Login"> </div>
       
 </form>
+</div>
