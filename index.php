519f8ca1b6b4762dae01e5ce4e7438b24fa0d296
pungki <pungky_abi@gmail.com>
2013-12-20 14:05:18 +0700

    membuat file index, login form

---
+++ index.php
@@ -0,0 +1,8 @@
+
+	<form class="form" method="post" action="proseslogin.php">
+	    <div> <h2>Login Admin</h2></div>
+		<div><input type="text" name="username" placeholder="Username" required></div>
+		 <div><input type="password" name="password" placeholder="Password" required> </div>
+	<div> <input type="submit" value="Login"> </div>
+      
+</form>
