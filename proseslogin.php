7d293dffc4a059b3ef343f2e5d58412cec16a1f7
pungki <pungky_abi@gmail.com>
2013-12-20 14:05:37 +0700

    membuat proses login

---
+++ proseslogin.php
@@ -0,0 +1,20 @@
+<?php
+include "konektor.php";
+$username=$_POST['username'];
+$password=$_POST['password'];
+$sql=mysql_query("SELECT * FROM user WHERE binary username='$username' AND binary password='$password' limit 1");
+
+$r=mysql_fetch_array($sql);
+
+if ($r){
+	 session_start();
+	 session_register("username");
+	 session_register("password");
+	 $_SESSION[username] = $r['username'];
+	 $_SESSION[password] = $r['password'];
+     header('location:index1.php');
+
+}else{
+?><script language="JavaScript">alert('Maaf username dan password yang Anda masukkan belum benar, Silahkan dicoba lagi'); document.location='index.php'</script><?php
+}
+?>
